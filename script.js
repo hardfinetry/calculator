4aa1c1d69fd07f7cb3c2d8d5ccb26aac3a5aea7e
hardfinetry-whitey <221923048+hardfinetry@users.noreply.github.com>
2026-06-01 21:10:53 +0800

    allow multi number calculations and add clear button

--- script.js
+++ script.js
@@ -1,55 +1,66 @@
-let number1;
-let number2;
+let number1 = 0;
+let number2 = 0;
 let operator;
 let result;
 const numberButtons = document.querySelector("#numberButtons");
 const operatorButtons = document.querySelector("#operatorButtons");
 const calculate = document.querySelector("#calculate");
 const display = document.querySelector("#display");
+const clear = document.querySelector("#clear");
 
 function operate(number1, operator, number2) {
     switch (operator) {
         case "+":
-            result = number1 + number2;
+            result = Number(number1) + Number(number2);
             break;
         case "-":
-            result = number1 - number2;
+            result = Number(number1) - Number(number2);
             break;
         case "*":
-            result = number1 * number2;
+            result = Number(number1) * Number(number2);
             break;
         case "/":
-            result = number1 / number2;
+            result = Number(number1) / Number(number2);
             break;
     }
     display.innerText += "=" + result;
     return result;
 };
 
-function assignNumber (click) {
-    if (number1 == undefined) {
-        number1 = Number(click.target.innerHTML);
-        display.innerText = number1;
-    }
-    else if (number2 ==undefined) {
-        number2 = Number(click.target.innerHTML);
-        display.innerText += number2;
+function assignNumber(click) {
+    if (click.target.matches(".buttons")) {
+        if (operator == undefined) {
+            number1 += click.target.innerHTML;
+            display.innerText += click.target.innerHTML;
+            console.log(click.target);
+        }
+        else {
+            number2 += click.target.innerHTML;
+            display.innerText += click.target.innerHTML;
+        }
     }
 };
 
-function assignOperator (click) {
-    if (operator == undefined) {
-        operator = click.target.innerHTML;
-        display.innerText += operator;
+function assignOperator(click) {
+    if (click.target.matches(".buttons")) {
+        if (operator == undefined) {
+            operator = click.target.innerHTML;
+            display.innerText += operator;
+        }
     }
-    console.log({operator});
 };
 
 numberButtons.addEventListener("click", assignNumber);
 operatorButtons.addEventListener("click", assignOperator);
+clear.addEventListener("click", () => {
+    display.innerText = "";
+    number1 = 0;
+    number2 = 0;
+    operator = undefined;
+})
 calculate.addEventListener("click", () => {
     operate(number1, operator, number2);
-    number1 = undefined;
-    number2 = undefined;
+    number1 = 0;
+    number2 = 0;
     operator = undefined;
 });
